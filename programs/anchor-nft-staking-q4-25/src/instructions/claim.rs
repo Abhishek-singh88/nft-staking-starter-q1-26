e4ee9da01edf8315d587cbacc9d99711c7992f7e
Abhishek Singh <abhisheksingh4928@gmail.com>
2026-01-29 17:05:31 +0530

    claiming

--- programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
+++ programs/anchor-nft-staking-q4-25/src/instructions/claim.rs
@@ -6,13 +6,70 @@ use anchor_spl::{
 
 use crate::state::{StakeConfig, UserAccount};
 
-// #[derive(Accounts)]
-// pub struct Claim<'info> {
-// //TODO
-// }
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Claim<'info> {
-//     pub fn claim(&mut self) -> Result<()> {
-//     //TODO
-//     }
-// }
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"rewards".as_ref(), config.key().as_ref()],
+        bump = config.rewards_bump,
+    )]
+    pub rewards_mint: Account<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = rewards_mint,
+        associated_token::authority = user,
+    )]
+    pub user_rewards_ata: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Claim<'info> {
+    pub fn claim(&mut self) -> Result<()> {
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"config",
+            &[self.config.bump],
+        ]];
+
+        let amount = (self.user_account.points as u64)
+            .checked_mul(10u64.pow(self.rewards_mint.decimals as u32))
+            .unwrap();
+
+        mint_to(
+            CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                MintTo {
+                    mint: self.rewards_mint.to_account_info(),
+                    to: self.user_rewards_ata.to_account_info(),
+                    authority: self.config.to_account_info(),
+                },
+                signer_seeds,
+            ),
+            amount,
+        )?;
+
+        self.user_account.points = 0;
+
+        Ok(())
+    }
+}
